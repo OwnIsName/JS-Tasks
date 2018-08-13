f5f3a44f31068119ea0d4f084e1b414bdb852473
Sergei M <9957040@gmail.com>
2018-08-13 11:47:11 +0300

    Module_5 Task_2

---
+++ Module_5/Task_2/t2m5.js
@@ -0,0 +1,29 @@
+console.log("t2m5")
+
+/*
+  Напишите функию getPropValues(arr, prop), принимающую 
+  параметры arr - массив, и prop - имя ключа в объекте. 
+  
+  Функция должна возвращать массив всех значений этого ключа из arr.
+  
+  PS: обязательно используйте функциональные методы массивов, никаких for!
+*/
+
+const guests = [
+  { name: "Mango", age: 20, isActive: true },
+  { name: "Poly", age: 18, isActive: false },
+  { name: "Ajax", age: 30, isActive: true },
+  { name: "Chelsey", age: 45, isActive: false }
+];
+
+const getPropValues = function(arr, props){
+  const result = arr.map(guest => guest[props]);
+  return result;
+};
+
+// Вызовы функции для проверки
+console.log( getPropValues(guests, "name") ); // ['Mango', 'Poly', 'Ajax', 'Chelsey']
+
+console.log( getPropValues(guests, "age") ); // [20, 18, 30, 45]
+
+console.log( getPropValues(guests, "isActive") ); // [true, false, true, false]
