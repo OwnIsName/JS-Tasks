16d31c2c7b08697ce107e0042ec8d917738fc471
Sergei <9957040@gmail.com>
2018-08-14 15:49:17 +0300

    Task 2 ready

---
+++ Module_6/Task_2/t2m6.js
@@ -0,0 +1,53 @@
+console.log("t2m6")
+
+/*
+  Напишите функцию-конструктор StringBuilder.
+  
+  На вход она получает один параметр string - строку.
+  
+  Добавьте следующие методы в prototype функции-конструктора.
+  
+    - getValue() - выводит в консоль текущее значение поля value
+  
+    - append(str) - получает парметр str - строку и добавляет 
+      ее в конец значения поля value
+    
+    - prepend(str) - получает парметр str - строку и добавляет 
+      ее в начало значения поля value
+  
+    - pad(str) - получает парметр str - строку и добавляет 
+      ее в начало и в конец значения поля value
+*/
+
+
+function StringBuilder(string = "") {
+  this.value = string;
+}
+
+StringBuilder.prototype.showValue = function(){
+  console.log(`Value is: ${this.value}`)
+}
+
+StringBuilder.prototype.append = function(str){
+  this.value = this.value.concat(str);
+}
+
+StringBuilder.prototype.prepend = function(str){
+  this.value = str.concat(this.value);
+}
+
+StringBuilder.prototype.pad = function(str){
+  this.append(str)
+  this.prepend(str)
+}
+
+const myString = new StringBuilder('.');
+
+myString.append('^'); 
+myString.showValue(); // '.^'
+
+myString.prepend('^'); 
+myString.showValue(); // '^.^'
+
+myString.pad('='); 
+myString.showValue(); // '=^.^='
