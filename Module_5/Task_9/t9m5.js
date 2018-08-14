f5fa492f1f1799b7b0a61c9a0f2752e9f9072781
Sergei <9957040@gmail.com>
2018-08-14 13:15:41 +0300

    Task_9, Full Module_5

---
+++ Module_5/Task_9/t9m5.js
@@ -0,0 +1,30 @@
+console.log("t9m5")
+
+/*     
+  Напишите функию allGuestsActive(guests), принимающую 
+  один параметр guests - массив объектов гостей. 
+  
+  Функция должна возвращать true если значение поля isActive 
+  всех объектов true, в противном случае false.
+  
+  PS: используйте метод every или some, никаких for!
+*/
+
+const guestsA = [
+  { name: "Mango", isActive: true },
+  { name: "Poly", isActive: false },
+  { name: "Ajax", isActive: true }
+];
+
+const guestsB = [
+  { name: "Mango", isActive: true },
+  { name: "Poly", isActive: true },
+  { name: "Ajax", isActive: true }
+];
+
+const allGuestsActive = guests => guests.every(guest => guest.isActive)
+
+// Вызовы функции для проверки
+console.log(allGuestsActive(guestsA)); // false
+
+console.log(allGuestsActive(guestsB)); // true
